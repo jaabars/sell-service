ad55832a1b3be4dfe95f2b14230c7f07b476f4da
jaabars <aziret3405@gmail.com>
2021-09-24 18:58:44 +0600

    created method getCode in UserController

--- src/main/java/kg/easy/sellservice/controllers/UserController.java
+++ src/main/java/kg/easy/sellservice/controllers/UserController.java
@@ -1,4 +1,23 @@
 package kg.easy.sellservice.controllers;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/user")
 public class UserController {
+
+
+    @PostMapping("/signIn")
+    public ResponseEntity<?> getCode(@RequestParam String login){
+        /*
+        * Реализовать получение 4-х значного кода на почту пользователя
+        *
+        * */
+        return null;
+    }
 }
